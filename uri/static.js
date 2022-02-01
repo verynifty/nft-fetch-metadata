5e764fc526729767a18a4324e5665081ba8ac56a
Adam H <surfcoderepeat@protonmail.com>
2022-02-01 00:18:25 -0600

    support for custo data uri

--- uri/static.js
+++ uri/static.js
@@ -18,27 +18,26 @@ exports.getStaticURI = (chainName, tokenAddress, tokenId) => {
 };
 
 exports.getURIData = (chainName, tokenAddress, tokenId) => {
-  // todo for later
-  //   if (
-  //     isAddressMatch(chainName, tokenAddress, WRAPPED_CRYPTOPUNKS_TOKEN_ADDRESS)
-  //   ) {
-  //     return Promise.resolve({
-  //       title: `W#${tokenId}`,
-  //       name: `W#${tokenId}`,
-  //       description:
-  //         "This Punk was wrapped using Wrapped Punks contract, accessible from https://wrappedpunks.com",
-  //       external_url: `https://larvalabs.com/cryptopunks/details/${tokenId}`,
-  //     });
-  //   }
-  //   if (isAddressMatch(chainName, tokenAddress, AUTOGLYPHS_TOKEN_ADDRESS)) {
-  //     return Promise.resolve({
-  //       title: `Autoglyph #${tokenId}`,
-  //       name: `Autoglyph #${tokenId}`,
-  //       image: `https://www.larvalabs.com/autoglyphs/glyphimage?index=${tokenId}`,
-  //       description:
-  //         "Autoglyphs are the first “on-chain” generative art on the Ethereum blockchain. A completely self-contained mechanism for the creation and ownership of an artwork.",
-  //       external_url: `https://www.larvalabs.com/autoglyphs/glyph?index=${tokenId}`,
-  //     });
-  //   }
+  if (
+    isAddressMatch(chainName, tokenAddress, WRAPPED_CRYPTOPUNKS_TOKEN_ADDRESS)
+  ) {
+    return Promise.resolve({
+      title: `W#${tokenId}`,
+      name: `W#${tokenId}`,
+      description:
+        "This Punk was wrapped using Wrapped Punks contract, accessible from https://wrappedpunks.com",
+      external_url: `https://larvalabs.com/cryptopunks/details/${tokenId}`,
+    });
+  }
+  if (isAddressMatch(chainName, tokenAddress, AUTOGLYPHS_TOKEN_ADDRESS)) {
+    return Promise.resolve({
+      title: `Autoglyph #${tokenId}`,
+      name: `Autoglyph #${tokenId}`,
+      image: `https://www.larvalabs.com/autoglyphs/glyphimage?index=${tokenId}`,
+      description:
+        "Autoglyphs are the first “on-chain” generative art on the Ethereum blockchain. A completely self-contained mechanism for the creation and ownership of an artwork.",
+      external_url: `https://www.larvalabs.com/autoglyphs/glyph?index=${tokenId}`,
+    });
+  }
   return;
 };
