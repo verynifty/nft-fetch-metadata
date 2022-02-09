f3a54aa1cd56792908f084a3b3fba5c4baa6563a
Adam H <surfcoderepeat@protonmail.com>
2022-02-08 19:31:54 -0600

    typo

--- uri/data.js
+++ uri/data.js
@@ -1,4 +1,4 @@
-exports.createDataURI(mime, data) {
+exports.createDataURI = function (mime, data) {
   const dataBuffer = Buffer.from(data, "utf-8");
   return `data:${mime};base64,${dataBuffer.toString("base64")}`;
-}
+};
